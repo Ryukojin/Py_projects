b03231dfa4bb6dc636dc81fc74f877cf077eadae
ryukojin <fahim2722@hotmail.com>
2020-02-27 03:56:15 +1100

    All functions working

---
+++ Backup_files/backup.py
@@ -0,0 +1,44 @@
+import os
+import shutil as sh
+
+
+source1 = r'c:/Users/Fahim/Downloads/Sin'
+dest1 = r'c:/Users/Fahim/Desktop/desttest/'
+#source2 = 
+#dest2 = 
+
+#FUNCTIONS
+#Initialise
+def prep(src,dst):
+    global ans
+    print("Files will be copied from ")
+    print(src + ' ==> ' + dst)
+    print("\nAre you sure? Type Y/N")
+    ans = input()
+
+#Transfer
+def xfer(srce, destn):
+    sh.rmtree(destn)
+    sh.copytree(srce, destn)
+
+#Print result
+def output(dst,src): 
+    if os.listdir(dst) == os.listdir(src):
+        print("\nAll files successfully copied from " + src + ' ==> ' + dst )
+    else:
+        print("\nFile transfer Unsuccessful!")
+
+#Segment 1
+prep(source1,dest1)
+if ans == "Y":
+    xfer(source1,dest1)
+    output(source1,dest1)
+else:
+    print("\nYou have decided not to copy this segment... \nProceeding to next segment...")
+
+
+
+
+
+
+
---
+++ file_rename/rename.py
@@ -0,0 +1,13 @@
+import os, re
+
+files = [os.listdir(r"C:\Users\Fahim\Downloads\Gang Starr - One Of The Best Yet (2019)")]
+songs = [i for i in files if i.endswith(".mp3")]
+
+#Single file
+
+
+
+#Multi file
+map()
+
+
