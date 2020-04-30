abd86c12bd56be4b968149ef285f95b5657ca5c2
ryukojin <fahim2722@hotmail.com>
2020-04-30 16:07:35 +1000

    April 30th revision 1

--- Backup_files/backup.py
+++ Backup_files/backup.py
@@ -10,14 +10,14 @@ dest2 = r'c:/Users/Fahim/Desktop/desttest2/'
 
 #Initialise
 def prep(src,dst):
-    global ans
     if os.path.isdir(dst) == True:
         print("\nFiles will be copied from ")
         print(src + ' ==> ' + dst)
-        ans = input("\nAre you sure to proceed? Type Y/N")
+        ans = input("\nAre you sure to proceed? Type Y/N\n")
     else:
-        print("\nDestination folder '" + dst + "' does not exist!")
+        print("\nWARNING: Destination folder '" + dst + "' does not exist!")
         ans = 'N'
+    return ans
 
 #Transfer
 def xfer(srce, destn):
@@ -33,13 +33,14 @@ def output(dst,src):
 
 # Main body
 def main(src,dst):
-    prep(src,dst)
+    ans = prep(src,dst)
     if ans == "Y":
         xfer(src,dst)
         output(src,dst)
         print("====================================================================================================================")
     else:
-        print("The segment will not be copied... \nProceeding to next segment...")
+        print("The segment will not be copied... \nProceeding to next segment...\n")
+        print("====================================================================================================================")
 
 if __name__ == "__main__":
     main(source1,dest1)
